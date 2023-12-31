a939fc93c9c8a478e3a2218707bf241e2da39c26
eslam reda <eslam.reda.div@gmail.com>
2023-12-31 09:58:48 +0200

    test

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dallei?prompt=' + prompt, {
+      const response = await fetch('https://threed-tshert.onrender.com/api/v1/dalle?prompt=' + prompt, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
